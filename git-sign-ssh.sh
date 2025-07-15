5abbccce2160b3479fc32219fb1bc4a524cef08f
Lev <79159094+Mopsgamer@users.noreply.github.com>
2025-07-15 18:53:14 +0200

    Create git-sign-ssh.sh

---
+++ git-sign-ssh.sh
@@ -0,0 +1,49 @@
+set -e
+
+trap 'echo "git sign setup has been interrupted"; exit 1' SIGINT
+
+git config --global gpg.format ssh
+
+email=$(git config --get user.email || true)
+if [ -z "$email" ]; then
+    echo "Seems like you haven't set up your git yet."
+    read -p "Enter your email address: " email
+    git config --global user.email "$email"
+    name=$(git config --get user.name || true)
+    if [ -z "$name" ]; then
+        read -p "Enter your name: " name
+        git config --global user.name "$name"
+    fi
+fi
+
+dir="$HOME/.ssh"
+mkdir -p "$dir"
+
+file="$dir/gitsign"
+path="$file.pub"
+
+ssh-keygen -t ed25519 -C "$(echo $email | xargs)" -N "" -f "$file"
+
+git config --global commit.gpgsign true
+git config --global user.signingkey "$path"
+
+if ! pgrep -x ssh-agent >/dev/null; then
+    eval "$(ssh-agent -s)"
+fi
+
+ssh-add "$file"
+
+if ! gh auth status >/dev/null 2>&1; then
+    echo "Seems like you haven't set up your gh yet."
+    gh auth login -w -h github.com -s admin:ssh_signing_key,repo,gist,workflow,read:org
+fi
+
+read -p "Enter your SSH key display name for GitHub (leave empty to skip signing key creation): " keyname
+if [ -n "$keyname" ]; then
+    gh ssh-key add "$path" --type signing --title "$keyname"
+    echo "SSH signing key added to GitHub."
+else
+    echo "Skipping SSH signing key upload to GitHub."
+fi
+
+echo "git sign setup has been completed"
